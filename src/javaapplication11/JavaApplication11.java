8b4b8187bc4743f847b5317afb8bff48eff425e4
unknown <you@example.com>
2020-10-16 23:16:06 +0600

    xyz

---
+++ src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,23 @@
+package javaapplication11;
+import java.util.Scanner;
+public class JavaApplication11 {
+       public static void main(String[] args) {
+ 
+        Scanner scan = new Scanner(System.in);
+        
+      System.out.println("    1\n");
+      System.out.println("   1    1\n");
+      System.out.println("  1   2    1\n");
+      System.out.println("1   3    3    1\n");
+      
+}
+         
+         
+         
+            
+      
+    
+}
+}
+
+    
